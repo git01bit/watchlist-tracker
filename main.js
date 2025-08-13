0cfc6dd909b7dd9b1648d234e8c343a1d97845bd
git01bit <majidpour018@gmail.com>
2025-08-13 20:32:19 +0330

    Implement local storage load functionality

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ let episodeCount = 0;
 let episodeStatus = [];
 
 // Set up event listeners for user interactions
+window.addEventListener("DOMContentLoaded", loadFromLocalStorage);
 createWatchlistBtn.addEventListener("click", createNewWatchlist);
 watchlistDetailsContainer.addEventListener("click", updateEpisodeStatus);
 deleteWatchlistBtn.addEventListener("click", deleteExistingWatchlist);
@@ -72,14 +73,16 @@ function deleteExistingWatchlist() {
 
   localStorage.clear();
 
+  // Display watchlist creation form
   creationWatchlistContainer.classList.remove("deactive");
   createWatchlistBtn.classList.remove("deactive");
 
+  // Hide Watchlist details view
   watchlistDetailsContainer.classList.add("deactive");
   deleteWatchlistBtn.classList.add("deactive");
 }
 
-// Saves the current state to localStorage on page close or refresh
+// Save current state to localStorage on page unload (close or refresh)
 function saveToLocalStorage() {
   title = document.getElementById("title").textContent;
   episodeCount = document.querySelectorAll(".episode");
@@ -95,5 +98,48 @@ function saveToLocalStorage() {
 
     localStorage.setItem("title", JSON.stringify(title));
     localStorage.setItem("episodeStatus", JSON.stringify(episodeStatus));
+  }
+}
+
+// Load saved state from localStorage on page load
+function loadFromLocalStorage() {
+  const localTitle = JSON.parse(localStorage.getItem("title"));
+  const localEpisodeStatus = JSON.parse(localStorage.getItem("episodeStatus"));
+
+  if (localTitle && localEpisodeStatus) {
+    // Hide watchlist creation form
+    creationWatchlistContainer.classList.add("deactive");
+    createWatchlistBtn.classList.add("deactive");
+
+    // Display Watchlist details view
+    watchlistDetailsContainer.classList.remove("deactive");
+    deleteWatchlistBtn.classList.remove("deactive");
+
+    h1Title.textContent = localTitle;
+
+    for (let i = 0; i < localEpisodeStatus.length; i++) {
+      console.log(localEpisodeStatus[i]);
+
+      if (localEpisodeStatus[i]) {
+        const episodeDiv = document.createElement("div");
+        episodeDiv.classList.add("episode");
+        episodeDiv.classList.add("seen");
+        episodeDiv.innerHTML += `
+          <p>قسمت ${i + 1}</p>
+          <button type="button">تماشا شده</button>
+        `;
+
+        watchlistDetailsContainer.appendChild(episodeDiv);
+      } else {
+        const episodeDiv = document.createElement("div");
+        episodeDiv.classList.add("episode");
+        episodeDiv.innerHTML += `
+          <p>قسمت ${i + 1}</p>
+          <button type="button">تماشا نشده</button>
+        `;
+
+        watchlistDetailsContainer.appendChild(episodeDiv);
+      }
+    }
   }
 }
